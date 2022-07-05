7d0588eb44ce1e73c73c733153b4d0a26aa6610e
unknown <yazi20012ti@student.nurulfikri.ac.id>
2022-07-05 11:43:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+1. Clone somewhere
+1. `npm install`
+1. Run `node <PATH_TO>/server.js`
+   * With env variable `PORT` set to whatever you want the port to be
+   * With a working directory for where you want the files to be
+1. (Optional) Migrate your games from the official Dropbox by calling the `http(s)://<url-to-server>/transfer/<game-id>` end point I added.
---
+++ server.js
@@ -0,0 +1,72 @@
+import express from 'express'
+import fs from 'fs'
+import fetch from 'node-fetch'
+
+const app = express();
+const port = 80;
+const filedir = process.cwd();
+const WRITE_OPTIONS = {
+	encoding: 'utf8',
+	mode: 0o660
+};
+
+(function configureMiddleware() {
+	app.use((req, res, next) => {
+		req.headers['content-type'] = req.headers['content-type'] || 'text/plain';
+		next()
+	});
+	app.use(express.raw({
+		type: `*/*`,
+		limit: `3MB`
+	}));
+})();
+
+(function configurePaths() {
+
+	app.get(`/isalive`, (req, res) => {
+		res.status(200).send(`true`)
+	});
+
+	app.use(`/files`, express.static(filedir));
+
+	app.put(`/files/:fileName`, (req, res) => {
+		try {
+			const fileName = req.params.fileName;
+			console.log(`Writing file ${fileName}, size ${req.body.length}`);
+			fs.writeFile(fileName, req.body, WRITE_OPTIONS, err => {
+				if (err) {
+					res.status(500).send(err.message);
+				} else {
+					res.status(200).send();
+				}
+			})
+		} catch (e) {
+			res.status(500).send(`${e}`);
+		}
+	});
+
+	app.get(`/transfer/:fileName`, async (req, res) => {
+		try {
+			const fileName = req.params.fileName;
+			console.log(`Transferring ${fileName} from Dropbox...`);
+			const dropbox = await fetch(`https://content.dropboxapi.com/2/files/download`, {
+				headers: {
+					"Dropbox-API-Arg": `{"path":"/MultiplayerGames/${fileName}"}`,
+					"Content-Type": `text/plain`,
+					Authorization: `Bearer LTdBbopPUQ0AAAAAAAACxh4_Qd1eVMM7IBK3ULV3BgxzWZDMfhmgFbuUNF_rXQWb`
+				}
+			});
+
+			console.log(`Success, writing file ${fileName}`);
+			await dropbox.body.pipe(fs.createWriteStream(fileName, WRITE_OPTIONS));
+
+			res.status(200).send();
+		} catch (e) {
+			res.status(500).send(`${e}`);
+		}
+	})
+})();
+
+app.listen(port, () => {
+	console.log(`Server listening on ${port}`);
+});
